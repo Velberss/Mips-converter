b63295ece6677022fd8b146620eaba17559cacf9
Pedro Ribas <pedrogarciaribas@hotmail.com>
2024-07-28 21:05:28 -0300

    fix: convertEspeciaisITipo

--- Mips/src/MIPSConverter.java
+++ Mips/src/MIPSConverter.java
@@ -3,39 +3,31 @@ import java.io.*;
 
 public class MIPSConverter {
     public static void main(String[] args) {
-        String entradaArq = "C:/Users/gonca/OneDrive/Área de Trabalho/🤓/Arqui/Mips/src/programa.txt";
-        String saidaArq = "C:/Users/gonca/OneDrive/Área de Trabalho/🤓/Arqui/Mips/src/output.txt";
-
+        String entradaArq = "Mips/src/programa3.txt";
+        String saidaArq = "Mips/src/output.txt";
         try {
             BufferedReader reader = new BufferedReader(new FileReader(entradaArq));
             BufferedWriter writer = new BufferedWriter(new FileWriter(saidaArq));
             String line;
-
             while ((line = reader.readLine()) != null) {
                 String binaryInstruction = converterToBinary(line);
                 writer.write(binaryInstruction);
                 writer.newLine();
             }
-
             reader.close();
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
     private static String converterToBinary(String instrucao) {
         String[] parts = instrucao.split("\\s+|,\\s*|\\(|\\)"); // o split separa a string e reparte cada informação
         String opcode = parts[0];
         String opcodeBinario = OpCodeMap.getOpcode(opcode);
-
         if (opcodeBinario == null) {
             throw new IllegalArgumentException("Opcode desconhecido!!");
         }
-
         switch (opcode) {
-
             case "add":
             case "sub":
             case "subu":
@@ -90,14 +82,12 @@ public class MIPSConverter {
     }
 
     private static String convertRTipo(String[] parts, String opcodeBinary) {
-
         {
             String rs = RegistradorMap.getRegistrador(parts[2]);
             String rt = RegistradorMap.getRegistrador(parts[3]);
             String rd = RegistradorMap.getRegistrador(parts[1]);
             String shamt = "00000"; // Valor padrão para R-Type
             String funct = parts[0].equals("add") ? "100000" : "100010";
-
             if (parts[0].equals("sll") || parts[0].equals("srl")) {
                 rd = RegistradorMap.getRegistrador(parts[1]);
                 rt = RegistradorMap.getRegistrador(parts[2]);
@@ -106,9 +96,7 @@ public class MIPSConverter {
             } else if (parts[0].equals("mult")) {
                 rd = "00000"; // RD é não utilizado
             }
-
             return opcodeBinary + rs + rt + rd + shamt + funct;
-
         }
     }
 
@@ -116,16 +104,11 @@ public class MIPSConverter {
         String rt = RegistradorMap.getRegistrador(parts[2]);
         String rs = RegistradorMap.getRegistrador(parts[1]);
         int imediato = Integer.parseInt(parts[3]);
-        String imediatoBinario = String.format("%16s", Integer.toBinaryString(imediato)).replace(' ', '0'); // Formata a
-                                                                                                            // variavel
-                                                                                                            // de string
-                                                                                                            // para
-                                                                                                            // binario
-
+        String imediatoBinario = String.format("%16s", Integer.toBinaryString(imediato & 0xFFFF)).replace(' ', '0');
         if (rs == null || rt == null) {
             throw new IllegalArgumentException("Registrador desconhecido");
         }
-
+    
         return opcodeBinary + rs + rt + imediatoBinario;
     }
 
